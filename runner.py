c5b24b4cb581dc2d17be161fbd3946f78e782ba5
shadzex <iv1123playball@gmail.com>
2022-11-20 01:11:18 +0900

    Modified

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ from viewer import Viewer
 
 import argparse
 
-from base import BaseRLAlgorithm, Hyperparameters
+from base import BaseRLAlgorithm, DistributedRunner, Hyperparameters
 from RL.env.env import Environment
 
 from logger import Logger
@@ -96,23 +96,26 @@ def default_setting(base_path, name, version):
 def run(algorithm_class):
     parser = argparse.ArgumentParser(description='Input Argument')
 
-    parser.add_argument('-e', '--env', type=str, default='Pendulum-v0')
-    parser.add_argument('-s', '--seed', type=int, default=0)
-    parser.add_argument('-v', '--version', type=int, default=-1)
-    parser.add_argument('-f', '--evaluate_freq', type=int, default=-1)
-    parser.add_argument('-m', '--max_iteration', type=int, default=1000000)
-    parser.add_argument('-x', '--explore_iteration', type=int, default=25000)
-    parser.add_argument('--train', action='store_true')
-    parser.add_argument('--view', action='store_true')
-    parser.add_argument('--best', action='store_true')
-    parser.add_argument('--test', action='store_true')
-    parser.add_argument('--render', action='store_true')
+    # Arguments
+    parser.add_argument('-e', '--env', type=str, default='Pendulum-v0') # Environment
+    parser.add_argument('-s', '--seed', type=int, default=0) # Seed
+    parser.add_argument('-v', '--version', type=int, default=-1) # Model version
+    parser.add_argument('-f', '--evaluate_freq', type=int, default=-1) # Save/evaluation frequency
+    parser.add_argument('-w', '--worker_num', type=int, default=4) # Worker number for distributed algorithms, disabled for non-distributed algorithms
+    parser.add_argument('-m', '--max_iteration', type=int, default=1000000) # Max iteration for training
+    parser.add_argument('-x', '--explore_iteration', type=int, default=25000) # First some iterations can be used for random exploration with random action
+    parser.add_argument('--train', action='store_true') # Decide to train
+    parser.add_argument('--view', action='store_true') # Decide to view the graph of the log
+    parser.add_argument('--best', action='store_true') # Decide to test with the model evaluated during training with the best score
+    parser.add_argument('--test', action='store_true') # Decide to test
+    parser.add_argument('--render', action='store_true') # Decide to render during training(Automatically true for the test)
 
     args = parser.parse_args()
 
     seed = args.seed
     version = args.version
     evaluate_freq = args.evaluate_freq
+    worker_num = args.worker_num
     max_iteration = args.max_iteration
     explore_iteration = args.explore_iteration
 
@@ -137,7 +140,11 @@ def run(algorithm_class):
                                 hyperparameters)
 
     if train:
-        algorithm.train(env, seed, log_path, model_path, evaluate_freq, max_iteration, explore_iteration, render=render)
+        # If the algorithm is distributed,
+        if isinstance(algorithm, DistributedRunner):
+            algorithm.train(env, seed, log_path, model_path, evaluate_freq, worker_num, max_iteration, explore_iteration, render=render)
+        else:
+            algorithm.train(env, seed, log_path, model_path, evaluate_freq, max_iteration, explore_iteration, render=render)
 
     if view:
         viewer = Viewer(log_path, 10)
