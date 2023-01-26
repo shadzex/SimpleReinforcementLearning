65b2d8a56ced246f4829d21c88d36ede2c105a3f
shadzex <iv1123playball@gmail.com>
2023-01-26 20:58:36 +0900

    Comments added

--- MFRL/DQN/DoubleDQN/DoubleDQN.py
+++ MFRL/DQN/DoubleDQN/DoubleDQN.py
@@ -21,7 +21,9 @@ class DoubleDQN(DQN):
 
                                         hyperparameters)
 
-    def get_target(self, rewards, dones, next_states):
+    def get_target(self, rewards, dones, next_states, *args):
+        # Equation (4) from the original paper
+        # Decouples the action selection for the action evaluation
         with torch.no_grad():
             target_actions = self.q(next_states).argmax(1, keepdim=True)
             y = self.reward_scale * rewards + self.discount_factor * dones * self.q_target(next_states).gather(1,target_actions).squeeze(1)
