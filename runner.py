2f48e16e23d6eaf141030861b3d1a8e94e5656d3
shadzex <iv1123playball@gmail.com>
2023-02-02 20:58:24 +0900

    Seed for gym must be nonnegative

--- runner.py
+++ runner.py
@@ -98,7 +98,7 @@ def run(algorithm_class):
 
     # Arguments
     parser.add_argument('-e', '--env', type=str, default='CartPole-v1') # Environment
-    parser.add_argument('-s', '--seed', type=int, default=0) # Seed
+    parser.add_argument('-s', '--seed', type=int, default=1) # Seed
     parser.add_argument('-v', '--version', type=int, default=-1) # Model version
     parser.add_argument('-f', '--evaluate_freq', type=int, default=-1) # Save/evaluation frequency
     parser.add_argument('-w', '--worker_num', type=int, default=4) # Worker number for distributed algorithms, disabled for non-distributed algorithms
@@ -139,10 +139,9 @@ def run(algorithm_class):
 
     if train:
         # If the algorithm is distributed,
-        if isinstance(algorithm, DistributedRunner):
-            algorithm.train(env, log_path, model_path, evaluate_freq, worker_num, max_iteration, explore_iteration, render=render)
-        else:
-            algorithm.train(env, log_path, model_path, evaluate_freq, max_iteration, explore_iteration, render=render)
+        if not isinstance(algorithm, DistributedRunner):
+            worker_num = 1
+        algorithm.train(log_path, model_path, evaluate_freq, worker_num, max_iteration, explore_iteration, render=render)
 
     if view:
         viewer = Viewer(log_path, 10)
